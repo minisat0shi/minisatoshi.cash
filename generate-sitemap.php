0f2301cb65ecfc5cb455cadc91079039a18e5e5d
minisatoshi <19712510+minisat0shi@users.noreply.github.com>
2025-03-11 10:50:12 -0400

    Auto-generate sitemap script + updating robots.txt

---
+++ generate-sitemap.php
@@ -0,0 +1,48 @@
+<?php
+$siteUrl = 'https://minisatoshi.cash';
+$dir = __DIR__;
+
+// Function to get HTML files, excluding specific ones
+function getHtmlFiles($dir) {
+    $files = [];
+    $exclude = ['index.html', '404.html', 'submit-to-bing.php', 'submit-to-google.php', 'generate-sitemap.php', 'resources.php'];
+    $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($dir));
+    foreach ($iterator as $file) {
+        if ($file->isFile() && preg_match('/\.html$/', $file->getFilename()) 
+            && !in_array($file->getFilename(), $exclude)) {
+            $files[] = $file->getPathname();
+        }
+    }
+    return $files;
+}
+
+// Generate URLs
+$htmlFiles = getHtmlFiles($dir);
+$urls = array_map(function($file) use ($siteUrl, $dir) {
+    $relativePath = str_replace($dir, '', $file);
+    $cleanPath = preg_replace('/\.html$/', '', $relativePath);
+    return $siteUrl . str_replace('\\', '/', $cleanPath);
+}, $htmlFiles);
+
+// Add home page
+if (!in_array("$siteUrl/", $urls)) {
+    $urls[] = "$siteUrl/";
+}
+
+// Build sitemap XML
+$xml = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+$xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
+foreach ($urls as $url) {
+    $xml .= "  <url>\n";
+    $xml .= "    <loc>$url</loc>\n";
+    $xml .= "    <lastmod>" . date('Y-m-d') . "</lastmod>\n";
+    $xml .= "    <changefreq>monthly</changefreq>\n";
+    $xml .= "    <priority>" . ($url === "$siteUrl/" ? "1.0" : "0.8") . "</priority>\n";
+    $xml .= "  </url>\n";
+}
+$xml .= '</urlset>';
+
+// Save to sitemap.xml
+file_put_contents("$dir/sitemap.xml", $xml);
+echo "Sitemap generated with " . count($urls) . " URLs.";
+?>
