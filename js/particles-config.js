23184f4b931079cd6708c8eeef05e4b4d9f681a2
minisatoshi <19712510+minisat0shi@users.noreply.github.com>
2025-04-09 15:09:58 -0400

    Cleaning JS -- moving to it's own JS file and moving to head

---
+++ js/particles-config.js
@@ -0,0 +1,57 @@
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 20,
+      "density": {
+        "enable": true,
+        "value_area":1000
+      }
+    },
+    "color": {
+      "value": ["#aa73ff", "#f8c210", "#83d238", "#33b1f8"]
+    },
+    "shape": {
+      "type": "image",
+      "stroke": {
+        "width": 1,
+        "color": "#fff"
+      },
+      "polygon": {
+        "nb_sides": 6
+      },
+      "image": {
+        "src": "images/Resources/Branding/PNG/bitcoin-cash-circle.png",
+        "width": 5,
+        "height": 5
+      }
+    },
+    "opacity": {
+      "value": 0.25,
+      "random": false,
+      "anim": {
+        "enable": false,
+        "speed": 1,
+        "opacity_min": 0.1,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 10,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 12,
+        "size_min": 0.1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": true,
+      "distance": 300,
+      "color": "#ffffff",
+      "opacity": 0.2,
+      "width": 1
+    },
+  },
+  "retina_detect": true
+});
